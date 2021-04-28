76bbf94b5cc2e174916ae8bb511601196eb8bf25
admin <mathieu.barrere@pregentum.com>
2021-04-28 14:01:36 +0000

    prod environment working with app1

---
+++ prod/apps/app1.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+import plotly.graph_objs as go
+from textwrap import dedent
+import numpy as np
+from app import app
+
+df = pd.read_csv("datasets/data_transformed.csv")
+all_location = df.location.dropna().unique()
+
+layout = html.Div([
+    html.H1('Dash App Basics',
+    ),
+
+    dcc.Markdown("Message à afficher"
+    ),
+
+    dcc.Dropdown(
+        id='country-dropdown',
+        options=[{'label': i, 'value': i} for i in all_location],
+        multi=True,
+        value=['Afghanistan']
+    ),
+
+    dcc.Graph(id='timeseries-graph')
+
+])
+
+@app.callback(
+    dash.dependencies.Output('timeseries-graph', 'figure'),
+    [dash.dependencies.Input('country-dropdown', 'value')])
+def update_graph(country_values):
+    dff = df.loc[df['location'].isin(country_values)]
+    return {
+        'data': [go.Scatter(
+            x=dff[dff['location'] == location]['date'],
+            y=dff[dff['location'] == location]['stringency_index'],
+            text="location ",
+            mode='lines',
+            name=location,
+            marker={
+                'size': 15,
+                'opacity': 0.5,
+                'line': {'width': 0.5, 'color': 'white'}
+            }
+        ) for location in dff.location.unique()],
+        'layout': go.Layout(
+            title="STI over time, by country",
+            xaxis={'title': 'date'},
+            yaxis={'title': 'stringency_index'},
+            margin={'l': 60, 'b': 50, 't': 80, 'r': 0},
+            hovermode='closest'
+        )
+    }
---
+++ prod/index.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 27 11:36:18 2021
+
+@author: meksi
+"""
+
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+# Connect to main app.py file
+from app import app
+from app import server
+
+# Connect to your app pages
+from apps import app1
+
+
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    html.Div([
+        dcc.Link("Page d'acceuil", href='/apps/app1'),
+    ], className="row"),
+    html.Div(id='page-content', children=[])
+])
+
+
+@app.callback(Output('page-content', 'children'),
+              [Input('url', 'pathname')])
+def display_page(pathname):
+    if pathname == '/apps/app1':
+        return app1.layout
+    else:
+        return "Welcome! Please choose a link"
+
+
+if __name__ == '__main__':
+    app.run_server(debug=False)
