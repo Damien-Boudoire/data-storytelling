d05b5806c717a7c60c33035ecad865566194b811
Olfa <meksiolfa@gmail.com>
2021-04-29 12:40:11 +0200

    mise à jour

--- app1.py
+++ app1.py
@@ -19,36 +19,6 @@ app = dash.Dash(__name__, external_stylesheets =[dbc.themes.DARKLY])
 
 all_location = df.Country.dropna().unique()
 
-# app.layout = html.Div([
-#     html.H1('Influence of restrictions on total deaths and cases of Covid-19', style={'text-align': 'center'}),
-#
-#     html.H5("Select at least two countries":),
-#
-#     dcc.Dropdown(
-#         id='country-dropdown',
-#         options=[{'label': i, 'value': i} for i in all_location],
-#         multi=True,
-#         value=['Afghanistan', "France"],
-#         clearable = False
-#     ),
-#
-#     html.Div([
-#         dcc.Graph(id='timeseries-graph', figure={},
-#                   config={
-#                       'staticPlot': False,  # True, False
-#                       'scrollZoom': True,  # True, False
-#                       'doubleClick': 'reset',  # 'reset', 'autosize' or 'reset+autosize', False
-#                       'showTips': True,  # True, False
-#                       'displayModeBar': True,  # True, False, 'hover'
-#                       'watermark': True,
-#                   }, className = "row"),
-#         html.Div([
-#             dcc.Graph(id="pie-graph", figure={}, className = "six columns"),
-#             dcc.Graph(id="pie-graph1", figure={}, className = "six columns")
-#         ], className = "row"),
-#         ])
-# ])
-
 app.layout = html.Div([
         dbc.Row(dbc.Col(html.H1('Influence of restrictions on total deaths and cases of Covid-19', style={'text-align': 'center'}),
                         ),
@@ -61,7 +31,8 @@ app.layout = html.Div([
             options=[{'label': i, 'value': i} for i in all_location],
             multi=True,
             value=['Afghanistan', "France"],
-            clearable = False
+            clearable = False,
+            style= { "hight": "100px",'color': '#212121', 'background-color': '#212121', "font-size":"24px"}
                                    ),
                         width={'size': 4, 'offset': 0},
                         ),
@@ -99,6 +70,7 @@ def update_graph(country_values):
     fig = px.line(dff, x='Date', y='Stringency_index', color='Country',
                   custom_data=['Country', 'Total_cases', 'Total_deaths', "Pop"])
     fig.update_traces(mode='lines+markers')
+    fig.update_layout(plot_bgcolor= 'rgb(17,17,17)', paper_bgcolor ='#3E4449', font_color="white", font_size= 16)
     return fig
 
 
@@ -113,6 +85,7 @@ def generate_chart(hov_data, country_values):
         dff1 = df[df.Country.isin(country_values)]
         dff1 = dff1[dff1.Date == "2020-10-11"]
         fig1 = px.pie(dff1, values="Total_cases", names="Country", color='Country')
+        fig1.update_layout(plot_bgcolor='rgb(17,17,17)', paper_bgcolor ='#3E4449', font_color="white")
         return fig1
     else:
         print(f'hover data: {hov_data}')
@@ -120,6 +93,7 @@ def generate_chart(hov_data, country_values):
         hov_date = hov_data['points'][0]['x']
         dff1 = dff1[dff1.Date == hov_date]
         fig2 = px.pie(dff1, values='Total_cases', names='Country', color='Country', title=f'Total cases: {hov_date}')
+        fig2.update_layout(plot_bgcolor='rgb(17,17,17)', paper_bgcolor ='#3E4449', font_color="white")
         return fig2
 
 @app.callback(
@@ -133,6 +107,7 @@ def generate_chart(hov_data, country_values):
         dff1 = df[df.Country.isin(country_values)]
         dff1 = dff1[dff1.Date == "2020-10-11"]
         fig1 = px.pie(dff1, values="Total_deaths", names="Country", color='Country')
+        fig1.update_layout(plot_bgcolor='rgb(17,17,17)', paper_bgcolor ='#3E4449', font_color="white")
         return fig1
     else:
         print(f'hover data: {hov_data}')
@@ -140,6 +115,7 @@ def generate_chart(hov_data, country_values):
         hov_date = hov_data['points'][0]['x']
         dff1 = dff1[dff1.Date == hov_date]
         fig2 = px.pie(dff1, values='Total_deaths', names='Country', color='Country', title=f'Total deaths: {hov_date}')
+        fig2.update_layout(plot_bgcolor='rgb(17,17,17)', paper_bgcolor ='#3E4449', font_color="white")
         return fig2
 
 if __name__ == '__main__':
@@ -168,3 +144,5 @@ if __name__ == '__main__':
 #             hovermode='closest'
 #         )
 #     }
+
+#"display":"inline-block"
